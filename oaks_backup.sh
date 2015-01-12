fb92cd398386566078fd097469c32dd36edcf3cf
Jordan Westhoff <jordanwesthoff@gmail.com>
2015-01-12 15:17:36 -0500

    Fixed syntax and added preamble

--- oaks_backup.sh
+++ oaks_backup.sh
@@ -3,6 +3,15 @@
 # Script to backup incremental and important features from the Oaks web directory and others on the server.
 # Jordan Westhoff, RIT 2015
 
+# Script seeks to accomplish 4 goals, in order
+#	1) Check for root, then move to the specified user, create temp_dir
+#	2) Find the DPhoto directories, tar them into a tar.gz and copy to temp_dir
+#	3) SCP that tar.gz from temp_dir to safe backup_server
+# 	4) Clean out the temp folder 
+
+
+
+#############################################################################
 # Check to make sure the script is being run as root. 
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root" 1>&2
@@ -29,7 +38,7 @@ chown -R jordan:jordan $BACKUP_NAME
 
 # Secure copy the backup to my main disk server. Passwordless SSH is only run through
 #	my specific user account - not su, so make sure to revert for secure and permissive SSH
-su jordan
+su $USER
 scp $BACKUP_NAME $USER@$BACKUP_SERVER.student.rit.edu:/mnt/Access1/Oaks >> scp_log.txt
 
 
@@ -37,6 +46,10 @@ scp $BACKUP_NAME $USER@$BACKUP_SERVER.student.rit.edu:/mnt/Access1/Oaks >> scp_l
 sleep 5
 
 
-# Let's get rid of the temp shenanigans
+# Let's get rid of the temp shenanigans - saves some space on the machine - don't need three copies
+# of copies of copies. ya know.
+cd /home/$USER
+rm -rf Oaks_temp
+
 
 
