0ec44eb65d5d263f5bc2644fa87cd66bab5e3dad
Jordan Westhoff <jordanwesthoff@gmail.com>
2015-01-12 15:13:47 -0500

    Adding to the script

--- oaks_backup.sh
+++ oaks_backup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Script to backup incremental and important features from the Oaks web directory and others on the server.
-
+# Jordan Westhoff, RIT 2015
 
 # Check to make sure the script is being run as root. 
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root" 1>&2
     exit 1
 fi
-
-
 
 # Allows a little flexibility until I hardcode the ability to designate user 
 # 	and others as a string input.
@@ -20,15 +18,25 @@ BACKUP_NAME='oaks_web_$TIME.tar.gz'
 
 
 # Make a new home for all of the intermediary files to live. Then go to it.
-#	mkdir -p /home/$USER/Oaks
-#	cd /home/$USER/Oaks
+mkdir -p /home/$USER/Oaks_temp
+cd /home/$USER/Oaks_temp
 
 # Let's start copying and moving directories around. Using tar (gz) to compress and save space. 
-tar -zcvf /home/jordan/$BACKUP_NAME /var/www/html/dphoto
+tar -zcvf $BACKUP_NAME /var/www/html/dphoto
 
 # Re-setting permissions because Prowler is a little weird about rooted files coming in. 
 chown -R jordan:jordan $BACKUP_NAME
 
-# Secure copy the backup to my main disk server
-scp $BACKUP_NAME $USER@$BACKUP_SERVER.student.rit.edu:/mnt/Access1/Oaks
+# Secure copy the backup to my main disk server. Passwordless SSH is only run through
+#	my specific user account - not su, so make sure to revert for secure and permissive SSH
+su jordan
+scp $BACKUP_NAME $USER@$BACKUP_SERVER.student.rit.edu:/mnt/Access1/Oaks >> scp_log.txt
+
+
+# Sometimes SCP hangs a little at the end when piping to log.
+sleep 5
+
+
+# Let's get rid of the temp shenanigans
+
 
