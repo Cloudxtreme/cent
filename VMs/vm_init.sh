e8941cfaf096f0bd2bb0b886b9f3dad0d161417e
Jordan <jordanwesthoff@gmail.com>
2015-03-30 20:45:33 -0400

    Created function calls

--- VMs/vm_init.sh
+++ VMs/vm_init.sh
@@ -9,23 +9,31 @@ if [ "$(id -u)" != "0" ]; then
 fi
 ################
 
-yum -y install openssh-server gcc make kernel-devel
-yum -y install dkms binutils patch libgomp glibc-headers glibc-devel kernel-headers
-yum -y update
-yum -y install git graphicsmagick imagemagick
+
+function install_update
+{
+  yum -y install openssh-server gcc make kernel-devel
+  yum -y install dkms binutils patch libgomp glibc-headers glibc-devel kernel-headers
+  yum -y update
+  yum -y install git graphicsmagick imagemagick
+}
 
 
+function git_creation
+{
+  cd /home/$user/Desktop
+    mkdir -p GitHub
+  cd /GitHub
+    git clone https://github.com/jordanwesthoff/cent
+    git clone https://github.com/jordanwesthoff/senior_thesis
+  cd /home/$user/Desktop
+
+  chown -R jordan:jordan /home/$user/Desktop/GitHub
+}
 
 
-cd /home/$user/Desktop
-mkdir -p GitHub
-cd /GitHub
-git clone https://github.com/jordanwesthoff/cent
-git clone https://github.com/jordanwesthoff/senior_thesis
-cd /home/$user/Desktop
-
-chown -R jordan:jordan /home/$user/Desktop/GitHub
-
-
-
-mkdir /mnt/SLURM
+function SLURM_mount
+{
+  mkdir -p /mnt/SLURM
+  mount -t vboxsf SLURM /mnt/SLURM
+}
