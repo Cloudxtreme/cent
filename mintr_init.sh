dd4629f082d4cd0c69665ed8032db4ecd9638e07
Jordan <jordanwesthoff@gmail.com>
2015-04-06 14:11:19 -0400

    Fully commented

--- mintr_init.sh
+++ mintr_init.sh
@@ -7,11 +7,14 @@ echo "Tooled for CentOS 6.x"
 echo " "
 echo " "
 
+
+# Check to make sure the user is ROOT or has super user proveleges
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root" 1>&2
     exit 1
 fi
 
+# CD to SRC, then fetch, unpack and install NODE.JS
 cd /usr/src
 wget http://nodejs.org/dist/v0.12.2/node-v0.12.2.tar.gz
 sleep 2
@@ -20,11 +23,10 @@ tar zxf node-v0.12.2.tar.gz
 cd node-v0.12.2
 ./configure
 make && make install
-
 sleep 5
 
 
-# Install NPM
+# Install NPM as a dependency for Mintr
 pkg="npm"
 if yum -qq -y install $pkg; then
     echo "Successfully installed $pkg"
@@ -33,6 +35,8 @@ else
 fi
 
 
+# Install Mintr via NPM
 sleep 5
 npm install -g mintr
 echo "Mintr installed"
+npm install 
