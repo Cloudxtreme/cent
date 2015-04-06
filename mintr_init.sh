58216c5a7bddb4bd6979723dec9e16d90591d97a
Jordan <jordanwesthoff@gmail.com>
2015-04-06 09:37:23 -0400

    Initial commit

---
+++ mintr_init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+```
+MINTR Install Tool
+Jordan Westhoff, 2015
+Tooled for CentOS 6.x
+```
+
+
+
+
+
+cd /usr/src
+wget http://nodejs.org/dist/v0.12.2/node-v0.12.2.tar.gz
+sleep 2
+
+tar zxf node-v0.12.2.tar.gz
