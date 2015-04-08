afc4b0d6ed3e96d62954fe64723cd594f9d6169e
Jordan Westhoff <jordanwesthoff@gmail.com>
2015-04-07 20:46:10 -0400

    initial commit

---
+++ VMs/permission_check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script is for Whisper VM's in order to make sure that they have write permissions on the shared directory.
+
+
+FULL=$(hostname).$(dnsdomainname)
+echo Hostname=$FULL
+
+touch /mnt/SLURM/vm_test/$Hostname.txt
