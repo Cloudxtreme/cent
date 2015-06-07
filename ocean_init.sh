7575480235f56baa6ab9cebadaca8da74d15fae0
Jordan Westhoff <jordanwesthoff@gmail.com>
2015-06-06 21:47:34 -0400

    Added basics

--- ocean_init.sh
+++ ocean_init.sh
@@ -1,2 +1,63 @@
 # Initialization script to bring up a Digital Ocean host
 
+
+
+
+
+
+
+
+
+
+
+
+# Body of the function
+
+#########################################
+pkg="mysql"
+if yum -qq install $pkg; then
+    echo "Successfully installed $pkg"
+else
+    echo "Error installing $pkg"
+fi
+#########################################
+
+#########################################
+pkg="mysql-server"
+if yum -qq install $pkg; then
+    echo "Successfully installed $pkg"
+else
+    echo "Error installing $pkg"
+fi
+#########################################
+
+#########################################
+pkg="php-xml"
+if yum -qq install $pkg; then
+    echo "Successfully installed $pkg"
+else
+    echo "Error installing $pkg"
+fi
+#########################################
+
+#########################################
+pkg="mysql"
+if yum -qq install $pkg; then
+    echo "Successfully installed $pkg"
+else
+    echo "Error installing $pkg"
+fi
+#########################################
+
+
+
+
+
+echo "Set HTTPD to start on boot."
+sudo chkconfig httpd on
+
+echo "Set MySQLd to start on boot."
+sudo chkconfig mysqld on
+
+echo "Set OpenSSH to start on boot."
+sudo chkconfig openssh-server on
